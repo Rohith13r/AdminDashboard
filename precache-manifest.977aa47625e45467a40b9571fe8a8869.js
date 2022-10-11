8489f8348449a91c5cbe3afa9390d14ff0dd6f03
Rohith13r <80285847+Rohith13r@users.noreply.github.com>
2022-10-11 16:28:24 +0530

    Updates

---
+++ precache-manifest.977aa47625e45467a40b9571fe8a8869.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab0931a343dbff039339",
+    "url": "/AdminDashboard/static/css/main.bed752da.chunk.css"
+  },
+  {
+    "revision": "ab0931a343dbff039339",
+    "url": "/AdminDashboard/static/js/main.957946b4.chunk.js"
+  },
+  {
+    "revision": "f93d64d6e13f42ce7d24",
+    "url": "/AdminDashboard/static/js/runtime~main.7959e759.js"
+  },
+  {
+    "revision": "3630631e4f9bbf9b1198",
+    "url": "/AdminDashboard/static/js/2.5823bb06.chunk.js"
+  },
+  {
+    "revision": "32972323a677ce6bfbdaa53f3a682d9f",
+    "url": "/AdminDashboard/static/media/avatar2.32972323.jpg"
+  },
+  {
+    "revision": "5992e5afad1aee076de9546407aa928d",
+    "url": "/AdminDashboard/static/media/avatar3.5992e5af.png"
+  },
+  {
+    "revision": "d0731a613852b167ce63c27fb8f6dac7",
+    "url": "/AdminDashboard/static/media/avatar4.d0731a61.jpg"
+  },
+  {
+    "revision": "a236461c803a93c95ec6ffddb00bf3da",
+    "url": "/AdminDashboard/static/media/product4.a236461c.jpg"
+  },
+  {
+    "revision": "395f7a511ecde338c48b0c0528b2240c",
+    "url": "/AdminDashboard/static/media/product5.395f7a51.jpg"
+  },
+  {
+    "revision": "d90017142f8de5b0ffbd0d6b63627031",
+    "url": "/AdminDashboard/static/media/product6.d9001714.jpg"
+  },
+  {
+    "revision": "a2285501c5c6c66be7298cb281b85c84",
+    "url": "/AdminDashboard/static/media/product7.a2285501.jpg"
+  },
+  {
+    "revision": "7503ee3afd09bd1feff870df74548f3a",
+    "url": "/AdminDashboard/static/media/product1.7503ee3a.jpg"
+  },
+  {
+    "revision": "92678d5b07efd485e487ca2b4c1b9bf3",
+    "url": "/AdminDashboard/static/media/product2.92678d5b.jpg"
+  },
+  {
+    "revision": "9848f98d7ffc4cffd7928016a408331a",
+    "url": "/AdminDashboard/static/media/product3.9848f98d.jpg"
+  },
+  {
+    "revision": "251686f618d15273ef2f4776ba084c72",
+    "url": "/AdminDashboard/static/media/product9.251686f6.jpg"
+  },
+  {
+    "revision": "ad576f7f908ec6819581643f64249fcb",
+    "url": "/AdminDashboard/static/media/product8.ad576f7f.jpg"
+  },
+  {
+    "revision": "056e87495e6313957eaa6f28b5dcb835",
+    "url": "/AdminDashboard/index.html"
+  }
+];
